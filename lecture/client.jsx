76dbc0c9f1340808ec5fe4a7d5e0a7fca1fc4be9
ehdrms519@naver.com <ehdrms519@naver.com>
2021-08-01 19:35:06 +0900

    Webpack

---
+++ lecture/client.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDom = require('react-dom')
+
+const WordReply = require('./WordReply');
+
+ReactDom.render(<WordReply /> , document.querySelector("#root"));
+
